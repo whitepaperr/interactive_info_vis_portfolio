6b1b9d04f67e70e60982bbe2aa35196bc6626e8c
haeun <haeuni437@gmail.com>
2026-02-02 17:26:24 -0800

    Sketch A: encode minutes as outer progress arc

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -13,12 +13,31 @@ registerSketch('sk2', function (p) {
 
   p.draw = function () {
     p.background(245);
-    p.noFill();
-    p.stroke(180);
-    p.strokeWeight(2);
 
+    // circle guide
+    p.noFill();
+    p.stroke(200);
+    p.strokeWeight(1.5);
     p.circle(cx, cy, rSecond * 2);
     p.circle(cx, cy, rMinute * 2);
     p.circle(cx, cy, rHour * 2);
+
+    // minute arc
+    const m = p.minute();
+    const angle = p.map(m, 0, 60, 0, p.TWO_PI);
+    p.noFill();
+    p.stroke(80, 140, 220); // blue
+    p.strokeWeight(8);
+    p.strokeCap(p.ROUND);
+
+    // start from top (12 o'clock)
+    p.arc(
+      cx,
+      cy,
+      rMinute * 2,
+      rMinute * 2,
+      -p.HALF_PI,
+      -p.HALF_PI + angle
+    );
   };
 });
